e9f16510ff0d0fc6057c1d5401fd6e623bfa3d73
UrielChanduvi <uriel_chanduvi@usmp.pe>
2025-09-26 22:10:00 -0500

    Corregir estructura y sintaxis para despliegue en Render8.0

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
+
 using Microsoft.Extensions.Caching.Distributed;
 using PortalInmobiliario.Data;
+using PortalInmobiliario.Models;
 
 namespace PortalInmobiliario.Controllers
 {
